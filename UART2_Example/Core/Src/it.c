cd99f4968f6f61ab17c036bbf1bcba9885d4a1a8
Serdar Gormez <hsgormez@gmail.com>
2024-08-06 02:11:11 +0300

    Added files for example of transmit and receive data with UART

---
+++ UART2_Example/Core/Src/it.c
@@ -0,0 +1,11 @@
+#include "main_app.h"
+#include "stm32f4xx_hal.h"
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler (void)
+{
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
