7fa59ad5498f7cd6358249768242fb9fa3118c84
Serdar Gormez <hsgormez@gmail.com>
2024-08-06 03:00:10 +0300

    Added files for example of interrupt with UART

---
+++ UART2_ExampleIT/Core/Src/it.c
@@ -0,0 +1,18 @@
+#include "main_app.h"
+#include "stm32f4xx_hal.h"
+
+extern UART_HandleTypeDef huart2;
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler (void)
+{
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
+
+void USART2_IRQHandler(void)
+{
+	HAL_UART_IRQHandler(&huart2);
+}
